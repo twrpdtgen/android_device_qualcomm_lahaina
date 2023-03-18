def2f9e1260af2f8889fb72f0fc64d8d69cad20f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-03-18 14:29:30 +0100

    lahaina: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: qualcomm Model: Lahaina for arm64 Script version: 3.0.0

---
+++ omni_lahaina.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from lahaina device
+$(call inherit-product, device/qualcomm/lahaina/device.mk)
+
+PRODUCT_DEVICE := lahaina
+PRODUCT_NAME := omni_lahaina
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := Lahaina for arm64
+PRODUCT_MANUFACTURER := qualcomm
+
+PRODUCT_GMS_CLIENTID_BASE := android-qualcomm
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lahaina-user 12 RKQ1.211119.001 1678897054433 release-keys"
+
+BUILD_FINGERPRINT := qti/lahaina/lahaina:12/RKQ1.211119.001/1678897054433:user/release-keys
